294e9db6d0db7d2a06d1885287904d8db0df1f22
joachimpatora <jpatora@excilys.com>
2014-03-12 11:05:17 +0100

    Spring Clean

--- src/main/webapp/validationForm.js
+++
@@ -1,9 +0,0 @@
-function validationForm()
-{
-	if ($("#nameInput").val()!="") {
-			$("#formAdd").submit();
-	}
-	else {
-		$("#message").html("Un ordinateur doit poss&eacute;der au moins un nom.");
-	}
-}
